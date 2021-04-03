cea230e260d111d01d609329ce2cc871e10f21e1
KayaTS <kayatarikseker@hotmail.com>
2021-04-03 03:33:27 +0300

    goturn py script

--- opencv2th-Video.py
+++ opencv2th-Video.py
@@ -2,6 +2,26 @@ import cv2
 import numpy as np
 import os
 
+clicked = False
+
+
+def onMouse(event, x, y, flags, param):
+    global clicked
+    if event == cv2.EVENT_LBUTTONUP:
+        clicked = True
+
+
+cameraCapture = cv2.VideoCapture(0)
+cv2.namedWindow('MyWindow')
+cv2.setMouseCallback('MyWindow', onMouse)
+print('Showing camera feed. Click window or press any key to stop.')
+success, frame = cameraCapture.read()
+while success and cv2.waitKey(1) == -1 and not clicked:
+    cv2.imshow('MyWindow', frame)
+    success, frame = cameraCapture.read()
+cv2.destroyWindow('MyWindow')
+cameraCapture.release()
+
 ''' ~Video copy
 videoCapture = cv2.VideoCapture('MyInputVid.avi')
 fps = videoCapture.get(cv2.CAP_PROP_FPS)
